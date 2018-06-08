ba669f5faadb183562d943708d1032711805231d
LeeHanYeong <dev@lhy.kr>
2018-06-08 16:18:04 +0900

    models.fields app추가, Person 클래스 구현

--- app/config/settings.py
+++ app/config/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'models.fields.apps.FieldsConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
