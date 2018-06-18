11380e05663335ca01a74c62576592d16cb91ae4
dohwan <dosio0102@gmail.com>
2018-06-18 15:24:46 +0900

    many_to_many InstagramUser

--- app/models/many_to_many/models/__init__.py
+++ app/models/many_to_many/models/__init__.py
@@ -1,6 +1,7 @@
 from .basic import *
 from .intermediate import *
 from .self import *
+from .self_symmetrical_false import *
 # models를 불러오기위해서 import시켜줘야 함
 # 원래는 models.py에서 migrate할 때 알아서 해주지만
 # models를 패키지로 만들었기 때문에 init파일에
---
+++ app/models/many_to_many/models/self_symmetrical_false.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+__all__ = (
+    'InstagramUser',
+)
+
+class InstagramUser(models.Model):
+    name = models.CharField(max_length=50)
+    following = models.ManyToManyField(
+        'self',
+        symmetrical=False,
+        related_name='followers'
+    )
