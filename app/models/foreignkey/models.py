9afaafe1a96d5e7c83877bf02351e28ce524d774
dohwan <dosio0102@gmail.com>
2018-06-18 11:44:01 +0900

    many to one

--- app/models/foreignkey/models.py
+++ app/models/foreignkey/models.py
@@ -1,19 +1,31 @@
 from django.db import models
 
 
-class Manufacturer(models.Model):
-    name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name
-
-
 class Car(models.Model):
     manufacturer = models.ForeignKey(
-        Manufacturer,
+        # 문자열 'Manufacturer'하는 이유
+        #  - 클래스 순서 관계없이 설정 가능
+        'Manufacturer',
         on_delete=models.CASCADE,
     )
     name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    instructer = models.ForeignKey(
+        'self',
+        on_delete=models.SET_NULL,
+        related_name='students',
+        blank=True,
+        null=True,
+    )
