7ab48d99aea00067a0add684ad7bc61b35252fef
dohwan <dosio0102@gmail.com>
2018-06-19 12:15:42 +0900

    many_to_many self symmetriacal

--- app/models/many_to_many/models/self_symmetrical_false_intermediate.py
+++ app/models/many_to_many/models/self_symmetrical_false_intermediate.py
@@ -17,6 +17,22 @@ class TwitterUser(models.Model):
     def __str__(self):
         return self.name
 
+# 두 프로퍼티에서 리턴하는 방식이 다르다
+# 둘이 어떻게 다른지 알자
+# block_relations에 써준 방식이 더 보기 쉽다
+    @property
+    def following(self):
+        # 내가 follow하고 있는 Relation들을 리턴
+        return Relation.objects.filter(from_user=self, relation_type='f')
+
+    @property
+    def block_relations(self):
+        return self.relations_by_from_user.filter(relation_type='b')
+
+    @property
+    def follower_relations(self):
+        return self.relations_by_to_user.filter(relation_type='f')
+
 
 class Relation(models.Model):
     CHOICE_RELATION_TYPE = (
